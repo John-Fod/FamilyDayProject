6013dffcfdde91096dbc686dab5fa8013f714ea8
Fodrocy John <jfodroc@terrasky.co.jp>
2018-07-19 09:52:34 +0900

    Pulled the updated data from my scratch org.

--- force-app/main/default/aura/einsteinVisionDemo/einsteinVisionDemoHelper.js
+++ force-app/main/default/aura/einsteinVisionDemo/einsteinVisionDemoHelper.js
@@ -1,12 +1,11 @@
 ({
+    scalingFactor:null,
     initializeData : function(component) {
         var action = component.get('c.initializeData');
         action.setCallback(this, function(response){
             var returnObject = JSON.parse(response.getReturnValue());
-            console.log('returnObject : ', returnObject);
             component.set('v.userId', returnObject.userId);
             component.set('v.datasets', returnObject.einsteinDatasets);
-            console.log("v.datasets : ", component.get('v.datasets'));
         });
         $A.enqueueAction(action);
     },
@@ -19,12 +18,14 @@
         $A.enqueueAction(action);
     },
     drawNewImage: function(component){
+        var self = this;
         var imageCanvas = component.find("imageCanvas");
         var ctx = imageCanvas.getElement().getContext('2d');
         var img = new Image();
         img.src = "data:image/jpg;base64," + component.get('v.imageInfo.versionData');
         img.onload = function(){
-            ctx.drawImage(img,0,0);
+            var scalingFactor = self.resizeCanvas(component, ctx, img);
+            ctx.drawImage(img, 0, 0, img.width * scalingFactor, img.height * scalingFactor);
         }
     },
     callEinsteinAPI: function(component, contentVersionId, imageModelId){
@@ -35,7 +36,25 @@
         });
         action.setCallback(this, function(response){
             console.log("response : ", response.getReturnValue());
+            var einsteinResponse = response.getReturnValue();
+            einsteinResponse.forEach(function(curLabel){
+                curLabel.percent = Math.floor(curLabel.probability * 10000) / 100;
+            })
+            component.set('v.einsteinResponse', einsteinResponse);
+            component.set('v.isLoading', false);
         });
+        component.set('v.isLoading', true);
         $A.enqueueAction(action);
+    },
+    /* Resize Canvas
+    DESC: Resize the canvas and save the results in the helper.scalingFactor attribute */
+    resizeCanvas: function(component, ctx, img){
+        var self = this;
+        var horizontalScalingFactor = ctx.canvas.parentElement.offsetWidth / img.width;
+        var verticalScalingFactor = ctx.canvas.parentElement.offsetHeight / img.height;
+        var scalingFactor = Math.max(horizontalScalingFactor, verticalScalingFactor);
+        ctx.canvas.width = img.width * scalingFactor;
+        ctx.canvas.height = img.height * scalingFactor;
+        return scalingFactor;
     }
 })
--- force-app/main/default/aura/familyDayAIDemo/familyDayAIDemo.app
+++
@@ -1,4 +0,0 @@
-<aura:application implements="force:appHostable">
-    <h1>Hello</h1>
-
-</aura:application>	
