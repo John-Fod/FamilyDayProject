558c0acb98b330de36e1ba8a280329570624d8a7
Fodrocy John <jfodroc@terrasky.co.jp>
2018-07-27 13:52:42 +0900

    Added basic support for Camera Capture

--- force-app/main/default/aura/CameraCaptureExampleComponent/CameraCaptureExampleComponentController.js
+++ force-app/main/default/aura/CameraCaptureExampleComponent/CameraCaptureExampleComponentController.js
@@ -1,5 +1,5 @@
 ({
-    doInit : function(component) {
+    doInit : function(component, event, helper) {
         var vfOrigin = "https://" + component.get("v.vfHost");
         window.addEventListener("message", function(event) {
             if (event.origin !== vfOrigin) {
@@ -8,8 +8,35 @@
             }
             // imageData属性にセットした上で、デバッグのためにConsoleにも出力
             component.set("v.imageData", event.data);
-            console.log('eventData : ', event.data);
+            helper.resizeFrame(component);
         }, false);
+    },
+    doRender : function(component, event, helper){
+        helper.resizeFrame(component);
+        /*
+        var vfFrame = component.find("vfFrame").getElement();
+        var vfParent = vfFrame.parentElement;
+        vfParent.height = vfFrame.scrollHeight;
+        vfFrame.width = vfFrame.parentElement.offsetWidth;
+        vfFrame.height = vfFrame.scrollHeight;
+        */
+        /*
+        var horizontalScalingFactor = ctx.canvas.parentElement.offsetWidth / img.width;
+        var verticalScalingFactor = ctx.canvas.parentElement.offsetHeight / img.height;
+        var scalingFactor = Math.max(horizontalScalingFactor, verticalScalingFactor);
+        ctx.canvas.width = img.width * scalingFactor;
+        ctx.canvas.height = img.height * scalingFactor;
+        */
+    },
+    onFrameLoaded : function(component, event, helper){
+        helper.resizeFrame(component);
+        /*
+        var vfFrame = component.find("vfFrame").getElement();
+        if(vfFrame){
+            vfFrame.height = "";
+            vfFrame.height = vfFrame.contentWindow.document.body.scrollHeight + "px";
+        }
+        */
     }
 
 })
--- force-app/main/default/aura/CameraCaptureExampleComponent/CameraCaptureExampleComponentHelper.js
+++ force-app/main/default/aura/CameraCaptureExampleComponent/CameraCaptureExampleComponentHelper.js
@@ -1,5 +1,7 @@
 ({
-    helperMethod : function() {
-
+    resizeFrame : function(component) {
+        var vfFrame = component.find("vfFrame").getElement();
+        vfFrame.width = vfFrame.parentElement.offsetWidth;
+        vfFrame.height = vfFrame.width * 0.75;
     }
 })
